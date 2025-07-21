628235d7d62c8a2baee49bb51429cbca27a8be64
Azz <arisachi7201@gmail.com>
2025-07-22 00:32:11 +0530

    last One

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App';
 import Frst from './components/Frst';
 import './index.css';
@@ -9,12 +9,12 @@ import Second from './components/Second';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/first" element={<Frst />} />
         <Route path="/second" element={<Second />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
